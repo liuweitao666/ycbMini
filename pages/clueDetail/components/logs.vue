2a98e003d8a6441dc5724357135dd361cf16c8f3
刘维韬 <1352819275@qq.com>
2021-10-19 21:07:16 +0800

    格式修复

--- pages/clueDetail/components/logs.vue
+++ pages/clueDetail/components/logs.vue
@@ -1,67 +1,70 @@
 <template>
 	<view class="logs_card">
-		<view class="followUp_log" v-if="data.length>0">
+		<view class="followUp_log" v-if="data.length > 0">
 			<view class="follow_item" v-for="item in data" :key="item.id">
-				<view class="custom_title">{{item.createUserName}}</view>
-				<view class="desc">{{item.createTime}}</view>
+				<view class="custom_title">{{ item.createUserName }}</view>
+				<view class="desc">{{ item.createTime }}</view>
 				<view class="desc">
-					<text style="color: #FC961E;padding-right: 10rpx">#{{followType[item.type] || item.operationType}}#</text>
-					{{item.followContent || item.operationType}}
+					<text style="color: #FC961E;padding-right: 10rpx">#{{ followType[item.type] || item.operationType }}#</text>
+					{{ item.followContent || item.operationType }}
 				</view>
 				<view class="image_cneg">
-					<image src="https://img1.baidu.com/it/u=3517333914,639949515&fm=26&fmt=auto" mode="widthFix"></image>
-					<image src="https://img1.baidu.com/it/u=3517333914,639949515&fm=26&fmt=auto" mode=""></image>
-					<image src="https://img1.baidu.com/it/u=3517333914,639949515&fm=26&fmt=auto" mode=""></image>
-					<image src="https://img1.baidu.com/it/u=3517333914,639949515&fm=26&fmt=auto" mode=""></image>
+					<image src="https://img1.baidu.com/it/u=3517333914,639949515&fm=26&fmt=auto" mode="widthFix" @click="previewImg"></image>
+					<image src="https://img1.baidu.com/it/u=3517333914,639949515&fm=26&fmt=auto" mode="" @click="previewImg"></image>
+					<image src="https://img1.baidu.com/it/u=3517333914,639949515&fm=26&fmt=auto" mode="" @click="previewImg"></image>
+					<image src="https://img1.baidu.com/it/u=3517333914,639949515&fm=26&fmt=auto" mode="" @click="previewImg"></image>
 				</view>
 			</view>
 		</view>
-		<view class="empty_view" v-if="total===0">
-			<u-empty text="暂无数据" mode="list"></u-empty>
-		</view>
+		<view class="empty_view" v-if="total === 0"><u-empty text="暂无数据" mode="list"></u-empty></view>
 	</view>
-
 </template>
 
 <script>
-	export default{
-		props:['data','total'],
-		data(){
-			return{
-				followType:[
-					'',
-					'电话',
-					'微信',
-					'旺旺',
-					'线下',
-					'其他',
-				]
-			}
-		},
-		created(){
-			console.log(this.data)
+export default {
+	props: ['data', 'total'],
+	data() {
+		return {
+			followType: ['', '电话', '微信', '旺旺', '线下', '其他']
+		};
+	},
+	methods: {
+		// 预览图片多张
+		previewImg() {
+			let _this = this;
+			let imgsArray = ['https://img1.baidu.com/it/u=3517333914,639949515&fm=26&fmt=auto','https://img1.baidu.com/it/u=3517333914,639949515&fm=26&fmt=auto','https://img1.baidu.com/it/u=3517333914,639949515&fm=26&fmt=auto'];
+			
+			uni.previewImage({
+				current: index - 1,
+				urls: imgsArray,
+				indicator: 'number',
+				loop: true
+			});
 		}
+	},
+	created() {
+		console.log(this.data);
 	}
+};
 </script>
 
 <style lang="scss" scoped>
 .logs_card {
-	padding:10rpx 0;
-	.followUp_log{
+	padding: 10rpx 0;
+	.followUp_log {
 		.follow_item {
 			padding: 0rpx 30rpx 20rpx;
-			border-bottom: 2rpx solid #EAEAEA;
+			border-bottom: 2rpx solid #eaeaea;
 			.custom_title {
-				padding-top:30rpx;
+				padding-top: 30rpx;
 				padding-bottom: 10rpx;
-				
 			}
-			.desc{
+			.desc {
 				line-height: 45rpx;
 			}
 			.image_cneg {
 				padding-top: 20rpx;
-				image{
+				image {
 					width: 120rpx;
 					height: 120rpx;
 					margin-right: 20rpx;
@@ -69,6 +72,5 @@
 			}
 		}
 	}
-
 }
 </style>
