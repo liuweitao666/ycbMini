ed8718135f4c24a743317b39efd645b2ea86a61c
刘维韬 <1352819275@qq.com>
2022-01-21 20:13:01 +0800

    bug#1616 客户详情页-手机号格式验证错误 易创宝 feature/V2.0

--- common/network/config.js
+++ common/network/config.js
@@ -14,9 +14,9 @@ if(process.env.NODE_ENV === 'development'){
     url_config.apiBaseUrl = 'http://19h517z135.iask.in'
 }else{
     // 生产环境
-    // url_config.baseUrl = 'https://erp.yichuangbao.com/api'
-		url_config.baseUrl = 'https://ycb-test1.yichuangbao.com/api'
-    url_config.apiBaseUrl = 'https://operate-service-api.vcrig.com'
+    url_config.baseUrl = 'https://erp.yichuangbao.com/api'
+		// url_config.baseUrl = 'https://ycb-test1.yichuangbao.com/api'
+  //   url_config.apiBaseUrl = 'https://operate-service-api.vcrig.com'
 }
 
 export default url_config
